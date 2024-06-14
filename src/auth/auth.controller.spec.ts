c795bed97514c1749e8195905fc6eeb859510eda
Maxrey88 <maximiliano.reyes@aicore.com.ar>
2024-06-14 14:01:17 -0300

    Add unit testing on auth controller and roles guard

---
+++ src/auth/auth.controller.spec.ts
@@ -0,0 +1,110 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { LoggerService } from '../common/custom-logger/logger.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from '../config/configuration';
+import typeorm from '../config/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRolesEnum } from '../user/enum/user-roles.enum';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { UserService } from '../user/user.service';
+import { LoginUserDto } from './dto/login-user.dto';
+import { BadRequestException, UnauthorizedException } from '@nestjs/common';
+
+describe('AuthController', () => {
+  let authController: AuthController;
+  let userService: UserService;
+
+  /* const mockAuthService = {
+    register: jest.fn(),
+    login: jest.fn(),
+    refreshToken: jest.fn(),
+    logout: jest.fn(),
+  } */
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          load: [configuration, typeorm],
+        }),
+        TypeOrmModule.forRootAsync({
+          inject: [ConfigService],
+          useFactory: async (configService: ConfigService) =>
+            configService.get('typeorm'),
+        }),
+        UserModule,
+        JwtModule.register({}),
+      ],
+      controllers: [AuthController],
+      providers: [LoggerService, AuthService],
+    }).compile();
+
+    authController = module.get<AuthController>(AuthController);
+    userService = module.get<UserService>(UserService);
+  });
+
+  it('should be defined', () => {
+    expect(authController).toBeDefined();
+  });
+
+  it('should register a new user and return accessToken and refreshToken', async () => {
+    const payload: RegisterUserDto = {
+      email: 'test1@example.com',
+      password: '123456',
+      name: 'test',
+      roles: [UserRolesEnum.USER],
+    };
+
+    const result = await authController.signup(payload);
+
+    expect(result).toHaveProperty('accessToken');
+    expect(result).toHaveProperty('refreshToken');
+  });
+
+  it('should login a user and return accessToken and refreshToken', async () => {
+    const payload: LoginUserDto = {
+      email: 'test1@example.com',
+      password: '123456',
+    };
+
+    const result = await authController.signin(payload);
+
+    expect(result).toHaveProperty('accessToken');
+    expect(result).toHaveProperty('refreshToken');
+  });
+
+  it('should throw an error if user already exists on register', async () => {
+    const payload: RegisterUserDto = {
+      email: 'test1@example.com',
+      password: '123456',
+      name: 'test',
+      roles: [UserRolesEnum.USER],
+    };
+
+    expect(authController.signup(payload)).rejects.toThrow(BadRequestException);
+  });
+
+  it('should throw an error if user not found on login', async () => {
+    const payload: LoginUserDto = {
+      email: 'test999@example.com',
+      password: '123456',
+    };
+
+    expect(authController.signin(payload)).rejects.toThrow(
+      UnauthorizedException,
+    );
+  });
+
+  afterEach(async () => {
+    jest.clearAllMocks();
+  });
+
+  afterAll(async () => {
+    await userService.deleteByEmail('test1@example.com');
+  });
+});
---
+++ src/common/guards/roles.guard.spec.ts
@@ -0,0 +1,67 @@
+import { ExecutionContext } from '@nestjs/common';
+import { RolesGuard } from './roles.guard';
+import { Reflector } from '@nestjs/core';
+import { Test, TestingModule } from '@nestjs/testing';
+
+describe('RolesGuard', () => {
+  let rolesGuard: RolesGuard;
+  let reflector: Reflector;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [RolesGuard, Reflector],
+    }).compile();
+
+    rolesGuard = module.get<RolesGuard>(RolesGuard);
+    reflector = module.get<Reflector>(Reflector);
+  });
+
+  it('should be defined', () => {
+    expect(rolesGuard).toBeDefined();
+    expect(reflector).toBeDefined();
+  });
+
+  it('should return true if no roles are required', () => {
+    const mockContext = createMockContext({});
+
+    jest.spyOn(reflector, 'getAllAndOverride').mockReturnValue(undefined);
+
+    expect(rolesGuard.canActivate(mockContext)).toBe(true);
+  });
+
+  it('should return true if user has required roles', () => {
+    const mockContext = createMockContext({ user: { roles: ['admin'] } });
+
+    jest.spyOn(reflector, 'getAllAndOverride').mockReturnValue(['admin']);
+
+    expect(rolesGuard.canActivate(mockContext)).toBe(true);
+  });
+
+  it('should return false if user does not have required roles', () => {
+    const mockContext = createMockContext({ user: { roles: ['user'] } });
+
+    jest.spyOn(reflector, 'getAllAndOverride').mockReturnValue(['admin']);
+
+    expect(rolesGuard.canActivate(mockContext)).toBe(false);
+  });
+
+  it('should return true if user has one almost required roles', () => {
+    const mockContext = createMockContext({
+      user: { roles: ['admin', 'user'] },
+    });
+
+    jest.spyOn(reflector, 'getAllAndOverride').mockReturnValue(['admin']);
+
+    expect(rolesGuard.canActivate(mockContext)).toBe(true);
+  });
+});
+
+function createMockContext(request: any): ExecutionContext {
+  return {
+    switchToHttp: () => ({
+      getRequest: () => request,
+    }),
+    getHandler: () => ({}),
+    getClass: () => ({}),
+  } as ExecutionContext;
+}
